3e34b0f836426b8c01f4bad3f72cc870663d76ad
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-04-24 22:14:54 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+      <div className="left">
+   <h2>Logo</h2>
+   </div>
+   <div className="right">
+ 
+    <button>Home</button>
+    <button>About</button>
+    <button>Menu</button>
+    <button>Order</button>
+    <button>Contact</button>
+    
+
+   </div>
+      </div>
+      <h1>Home</h1>
+   
+   <p>Made with 💖 by PJ</p>
+
+    </div>
+  );
+}
+
+export default App;
